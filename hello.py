ce72e1b87af7cfcce2a091c214008ab1f91bbf80
Ricardo Heredia <ricardoheredia94@gmail.com>
2025-02-06 21:15:02 +0100

    changes in files and deleted others

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from retail-forecasting!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ paths.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import os
+
+PARENT_DIR = Path(__file__).parent.resolve()
+DATA_DIR = PARENT_DIR / 'data'
+RAW_DATA_DIR = PARENT_DIR / 'data' / 'raw'
+TRANSFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
+DATA_CACHE_DIR = PARENT_DIR / 'data' / 'cache'
+
+MODELS_DIR = PARENT_DIR / 'models'
+
+ASSETS_DIR = PARENT_DIR / 'assets'
+
+if not Path(DATA_DIR).exists():
+    os.mkdir(DATA_DIR)
+
+if not Path(RAW_DATA_DIR).exists():
+    os.mkdir(RAW_DATA_DIR)
+
+if not Path(TRANSFORMED_DATA_DIR).exists():
+    os.mkdir(TRANSFORMED_DATA_DIR)
+
+if not Path(MODELS_DIR).exists():
+    os.mkdir(MODELS_DIR)
+
+if not Path(DATA_CACHE_DIR).exists():
+    os.mkdir(DATA_CACHE_DIR)
+
+if not Path(ASSETS_DIR).exists():
+    os.mkdir(ASSETS_DIR)
